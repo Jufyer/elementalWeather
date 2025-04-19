8b6d5c01422ccecee76253a343d944f27ba3c45d
Jufyer <julianders@gmx.net>
2025-04-19 11:41:14 +0200

    .

---
+++ src/main/java/org/jufyer/plugin/mobCoins/Main.java
@@ -0,0 +1,23 @@
+package org.jufyer.plugin.mobCoins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+  private static Main instance;
+
+  public static Main getInstance() {
+    return instance;
+  }
+
+  @Override
+  public void onEnable() {
+    instance = this;
+
+  }
+
+  @Override
+  public void onDisable(){
+
+  }
+}
